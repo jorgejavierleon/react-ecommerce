7d4e4d4918eebd95551916feb6167ce9df741c1a
Javier <jorgejavierleon@gmail.com>
2020-05-08 18:12:19 -0400

    sign in form

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component'
+
 
 function App() {
   return (
@@ -14,6 +16,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage}></Route>
         <Route path='/shop' component={ShopPage}></Route>
+        <Route path='/signin' component={SignInAndSignUp}></Route>
       </Switch>
     </div>
   );
